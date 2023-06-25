517f5b0f1eef76e6d82feba4b10f9e9d86d78483
Zachary <ramsumairzachary@gmail.com>
2023-06-24 20:41:59 -0400

    dist should show on frontend github

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -84,11 +84,11 @@ const start = async () => {
 			app.use("/api/v1/stripe", stripeRouter);
 
 		if (process.env.NODE_ENV === "production") {
-			app.use(express.static(path.join(__dirname, "../frontend/dist")));
+			app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 
 			app.get("*", (req, res) =>
 				res.sendFile(
-					path.resolve(__dirname, "../", "frontend", "dist", "index.html")
+					path.resolve(__dirname, "../../", "frontend", "dist", "index.html")
 				)
 			);
 		} else {
